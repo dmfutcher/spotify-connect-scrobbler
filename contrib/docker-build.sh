7d2c2170f1e35700208f52784c9b9a5c6f87743d
David Futcher <david.futcher@skyscanner.net>
2017-02-22 17:54:56 +0000

    Remove cache/ contrib/ dirs

--- contrib/docker-build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eux
-
-cargo build --release --no-default-features --features alsa-backend
-cargo build --release --target arm-unknown-linux-gnueabihf --no-default-features --features alsa-backend
-cargo build --release --target arm-unknown-linux-gnueabi --no-default-features --features alsa-backend
